99cde0ba8b55c278c3cfe58d328aeb2f973259d9
Luis M. Blanco <lblaseg246@g.educaand.es>
2022-01-24 14:06:12 +0100

    commit inicial

---
+++ src/main/java/com/luis/App.java
@@ -0,0 +1,13 @@
+package com.luis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
